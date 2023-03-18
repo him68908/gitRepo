23f8714e7ffe0f124054dddbd1c57e4ef2567517
him68908 <himanshu68908@gmail.com>
2023-03-18 17:52:46 +0530

    Updated All

--- Multiply.java
+++ Multiply.java
@@ -1,7 +1,7 @@
 public class Multiply {
-    public static void main() {
+    public static void main(String args[]) {
         int a = 10;
         int b = 20;
-        System.out.println("Product is" + (a * b));
+        System.out.println("Product is " + (a * b));
     }
 }
--- Test.java
+++ Test.java
@@ -1,7 +1,7 @@
 public class Test {
-    public static void main() {
+    public static void main(String args[]) {
         int a = 10;
         int b = 20;
-        System.out.println("Sum is" + (a + b));
+        System.out.println("Sum is " + (a + b));
     }
 }
